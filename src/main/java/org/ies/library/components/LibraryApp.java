a8e339c0506a43b3c290ff02c1ced67526daeafc
Carol <carol@b25>
2025-05-19 10:19:54 +0200

    ejercicio continuacion

--- src/main/java/org/ies/library/components/LibraryApp.java
+++ src/main/java/org/ies/library/components/LibraryApp.java
@@ -3,6 +3,7 @@ package org.ies.library.components;
 
 import lombok.AllArgsConstructor;
 import lombok.extern.log4j.Log4j;
+import org.ies.library.exceptions.BookNotFoundException;
 import org.ies.library.model.Library;
 
 import java.util.InputMismatchException;
@@ -15,17 +16,35 @@ public class LibraryApp {
     private final Scanner scanner;
     private final Library library;
 
-    public LibraryApp(Scanner scanner) {
+    public LibraryApp(Scanner scanner, Library library) {
         this.scanner = scanner;
+        this.library = library;
     }
+
 
     public void run(){
         int option;
         do{
             option = chooseOption();
             if(option ==1){
-                var books = library.showAllBooks();
-                log.info(books);
+                library.showAllBooks();
+
+            }else if (option == 2){
+                library.showAllMember();
+            }else if (option ==3){
+                try {
+                    log.info("Introduce el ISBN de libro que quieres buscar: ");
+                    String isbn = scanner.nextLine();
+
+                    var book = library.foundBookByIsbn(isbn);
+                    log.info(book);
+                }catch (BookNotFoundException e){
+                    log.error(e.getMessage());
+                }
+            }else if (option == 4){
+                try {
+                    log.info("Introduce el nif para buscar al socio: ");
+                }
             }
         }
 
